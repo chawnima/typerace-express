b6ba83bf487ee410804aae1f05178a986b0a95fc
chawnima <mferdyalviansyah@gmail.com>
2025-02-14 14:54:54 +0700

    cors

--- index.ts
+++ index.ts
@@ -5,15 +5,16 @@ import router from "./src/routes";
 import { errorHandler, notFoundHandler } from "./src/middlewares/error";
 
 const app = express();
-const PORT = process.env.PORT;
-const allowedOrigins = process.env.ORIGIN_URL;
+const PORT = Number(process.env.PORT) || 3000;
+const HOST_ADDRESS = process.env.HOST_ADDRESS || "localhost";
+const allowedOrigins = process.env.ORIGIN_URL?.split(",") || [];
 
-app.use(cors({ origin: allowedOrigins }));
 app.use(express.json());
+app.use(cors({ origin: allowedOrigins }));
 app.use("/", router);
 app.use("*", notFoundHandler);
 app.use(errorHandler);
 
-app.listen(PORT, () => {
+app.listen(PORT, HOST_ADDRESS, () => {
   console.log(`Server running on port ${PORT}`);
 });
