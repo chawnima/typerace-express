652f5c745803bab323e5d200d775f27dd57a4e1f
chawnima <mferdyalviansyah@gmail.com>
2025-03-24 14:18:29 +0700

    socket io test

--- src/middlewares/socket.ts
+++ src/middlewares/socket.ts
@@ -1,1 +1,1 @@
-const 
+export default {}
--- src/socket.ts
+++ src/socket.ts
@@ -3,51 +3,58 @@ import { Server, Socket } from "socket.io";
 
 export class SocketServer {
   public io: Server;
-  private role: string[] = ["room-master","player"];
+  private role: string[] = ["room-master", "player"];
 
   constructor(server: Express.Application) {
     this.io = new Server(server, { cors: { origin: "*" } });
     this.io.on("connection", this.StartListeners);
     this.io.of("/").adapter.on("join-room", (room, id) => {
       console.log(`socket ${id} joined room ${room}`);
+      const player = this.io.sockets.adapter.rooms.get(room);
+      console.log(player);
     });
   }
 
   StartListeners = (socket: Socket) => {
-    const userLog = (username: string = "") => {
+    const userLog = () => {
       console.log(
-        `connected. Online users = ${this.io.engine.clientsCount}. User Id = ${username} | ${
-          socket.id
-        }. Current room = ${Array.from(socket.rooms)} `
+        `connected. Online users = ${
+          this.io.engine.clientsCount
+        }. User Id = ${socket.handshake.auth.username} | ${socket.id}. Current room = ${Array.from(
+          socket.rooms
+        )} `
       );
     };
 
     // Track message handlers for each room
     const messageHandlers = new Map<string, (msg: any) => void>();
 
-    socket.on("room-id", (id,username: string) => {
-      const eventName = `message-${id}`;
+    socket.on("room-id", (id,  role: string = "player") => {
+      const eventName = [`message-${id}`];
 
       // Remove existing listener for this room (if any)
       const existingHandler = messageHandlers.get(id);
       if (existingHandler) {
-        socket.off(eventName, existingHandler); // Pass both event name AND handler
+        socket.off(eventName[0], existingHandler); // Pass both event name AND handler
         messageHandlers.delete(id);
       }
 
       // Join the room
       socket.join(id);
-      userLog(username);
+      userLog();
 
       // Create new handler for this room
-      const newHandler = (msg: any) => {
+      const messageHandler = (msg: any) => {
         this.io.to(id).emit(id, msg); // Send to room only
         console.log(`Message: ${msg} from ${socket.id}`);
       };
 
       // Add the new handler and save it
-      socket.on(eventName, newHandler);
-      messageHandlers.set(id, newHandler);
+      socket.on(eventName[0], messageHandler);
+      socket.on("player-count", (count) =>
+        this.io.to(id).emit("player-count", { count, name : this.io.sockets.adapter.rooms})
+      );
+      messageHandlers.set(id, messageHandler);
 
       socket.on("disconnect", () => {
         socket.leave(id);
